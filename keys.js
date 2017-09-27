fa317dbeb30b078ad813051ec0bc6a961b84af77
Whit <whitneyrscott@iMac-4.local>
2017-09-26 18:03:22 -0700

    third initial upload

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'POjqV9Mxvx8KmuxWntXeEsso9',
+  consumer_secret: 'JxLYKKw1dvzu8tVovYeLRy1PJm3Sxy6i1DfLfuo3Dk9je05nZG',
+  access_token_key: '912829519786008576-JZ1humDPu53pPrvhqHLgWMEKojjq1ef',
+  access_token_secret: 'UfuO1Ru1RqTam86GoQLFprURmcaqcJpKVDM0UoaaYRt9x',
+}
+
+module.exports = twitterKeys;
