1afabbc653fe8451c16e9b29cef446912ba99c7d
Whit <whitneyrscott@iMac-4.local>
2017-09-28 15:53:14 -0700

    Final Push

--- liri.js
+++ liri.js
@@ -1,18 +1,18 @@
-//Grab data from keys.js
+
 var keys = require('./keys.js');
 var request = require('request');
-var twitter = require('twitter');
+var Twitter = require('twitter');
 var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotifyKeys);
-var client = new twitter(keys.twitterKeys);
+var client = new Twitter(keys.twitterKeys);
 var fs = require('fs');
 
-//Stored argument's array
+
 var nodeArgv = process.argv;
 var command = process.argv[2];
-//movie or song
+
 var x = "";
-//attaches multiple word arguments
+
 for (var i=3; i<nodeArgv.length; i++){
   if(i>3 && i<nodeArgv.length){
     x = x + "+" + nodeArgv[i];
@@ -53,18 +53,14 @@ switch(command){
 }
 
 function showTweets(){
-  //Display last 20 Tweets
-  var screenName = {screen_name: 'SepersonFal'};
-  client.get('statuses/user_timeline', screenName, function(error, tweets, response){
+ 
+  var params = {screen_name: 'sepersonfal'};
+  client.get('statuses/user_timeline', params, function(error, tweets, response){
     if(!error){
       for(var i = 0; i<tweets.length; i++){
         var date = tweets[i].created_at;
-        console.log("@SepersonFal: " + tweets[i].text + " Created At: " + date.substring(0, 19));
+        console.log("@sepersonfal: " + tweets[i].text + " Created At: " + date.substring(0, 19));
         console.log("-----------------------");
-        
-        //adds text to log.txt file
-        fs.appendFile('log.txt', "@SepersonFal: " + tweets[i].text + " Created At: " + date.substring(0, 19));
-        fs.appendFile('log.txt', "-----------------------");
       }
     }else{
       console.log('Error occurred');
@@ -73,26 +69,15 @@ function showTweets(){
 }
 
 function spotifySong(song){
-  node-spotify-api.search({ type: 'track', query: song}, function(error, data){
+  spotify.search({ type: 'track', query: song}, function(error, data){
     if(!error){
       for(var i = 0; i < data.tracks.items.length; i++){
         var songData = data.tracks.items[i];
-        //artist
         console.log("Artist: " + songData.artists[0].name);
-        //song name
         console.log("Song: " + songData.name);
-        //spotify preview link
         console.log("Preview URL: " + songData.preview_url);
-        //album name
         console.log("Album: " + songData.album.name);
         console.log("-----------------------");
-        
-        //adds text to log.txt
-        fs.appendFile('log.txt', songData.artists[0].name);
-        fs.appendFile('log.txt', songData.name);
-        fs.appendFile('log.txt', songData.preview_url);
-        fs.appendFile('log.txt', songData.album.name);
-        fs.appendFile('log.txt', "-----------------------");
       }
     } else{
       console.log('Error occurred.');
@@ -115,19 +100,6 @@ function omdbData(movie){
       console.log("Language: " + body.Language);
       console.log("Plot: " + body.Plot);
       console.log("Actors: " + body.Actors);
-      
-
-      //adds text to log.txt
-      fs.appendFile('log.txt', "Title: " + body.Title);
-      fs.appendFile('log.txt', "Release Year: " + body.Year);
-      fs.appendFile('log.txt', "IMdB Rating: " + body.imdbRating);
-      fs.appendFile('log.txt', "Rotten Tomatoes Rating: " + body.tomatoRating);
-      fs.appendFile('log.txt', "Country: " + body.Country);
-      fs.appendFile('log.txt', "Language: " + body.Language);
-      fs.appendFile('log.txt', "Plot: " + body.Plot);
-      fs.appendFile('log.txt', "Actors: " + body.Actors);
-      
-
 
     } else{
       console.log('Error occurred.')
@@ -137,10 +109,6 @@ function omdbData(movie){
       console.log("If you haven't watched 'Mr. Nobody,' then you should: http://www.imdb.com/title/tt0485947/");
       console.log("It's on Netflix!");
 
-      //adds text to log.txt
-      fs.appendFile('log.txt', "-----------------------");
-      fs.appendFile('log.txt', "If you haven't watched 'Mr. Nobody,' then you should: http://www.imdb.com/title/tt0485947/");
-      fs.appendFile('log.txt', "It's on Netflix!");
     }
   });
 
